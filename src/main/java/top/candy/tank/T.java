f2877cb612c80c6fcf619bee4555b998c6352203
itchenp <itchenp@gmail.com>
2020-06-20 23:33:13 +0800

    第一版本git

---
+++ src/main/java/top/candy/tank/T.java
@@ -0,0 +1,23 @@
+package top.candy.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setSize(800,600);
+        f.setResizable(false);
+        f.setTitle("Tank War");
+        f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
