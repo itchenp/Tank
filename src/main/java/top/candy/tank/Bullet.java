88067e41eda7fd5d5165fcdce1f264bae410398d
itchenp <itchenp@gmail.com>
2020-06-29 00:05:53 +0800

    定义子弹类，设定属性

--- src/main/java/top/candy/tank/Bullet.java
+++ src/main/java/top/candy/tank/Bullet.java
@@ -1,4 +1,38 @@
 package top.candy.tank;
 
+import java.awt.*;
+
+/**
+ * 子弹
+ */
 public class Bullet {
+    private static final int SPEED = 10;
+
+    private static int WIDTH = 5,HIGHT = 5;
+
+    private int x,y;
+    private Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x,y,WIDTH,HIGHT);
+        g.setColor(c);
+        move();
+    }
+
+    private void move() {
+        switch (dir){
+            case RIGHT: x += SPEED;break;
+            case LEFT: x -= SPEED;break;
+            case DOWN: y += SPEED;break;
+            case UP: y -= SPEED;break;
+        }
+    }
 }
--- src/main/java/top/candy/tank/TankFrame.java
+++ src/main/java/top/candy/tank/TankFrame.java
@@ -9,6 +9,7 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     Tank tank = new Tank(200,200,Dir.DOWN);
+    Bullet bullet = new Bullet(300,300,Dir.DOWN);
 
     public TankFrame() throws HeadlessException {
         setSize(800,600);
@@ -29,6 +30,7 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         tank.paint(g);
+        bullet.paint(g);
     }
 
     class MyKeyListener extends KeyAdapter{
