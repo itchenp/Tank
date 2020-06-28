3b199855cec5e7b00946286157c953edbdbd50e3
itchenp <itchenp@gmail.com>
2020-06-29 00:46:45 +0800

    解决闪烁问题:刷新显存

--- src/main/java/top/candy/tank/TankFrame.java
+++ src/main/java/top/candy/tank/TankFrame.java
@@ -10,9 +10,10 @@ public class TankFrame extends Frame {
 
     Tank tank = new Tank(200,200,Dir.DOWN);
     Bullet bullet = new Bullet(300,300,Dir.DOWN);
+    static final int GAME_WIDTH = 800,GAME_HEIGTH = 600;
 
     public TankFrame() throws HeadlessException {
-        setSize(800,600);
+        setSize(GAME_WIDTH,GAME_HEIGTH);
         setResizable(false);
         setTitle("Tank War");
         setVisible(true);
@@ -26,6 +27,23 @@ public class TankFrame extends Frame {
             }
         });
     }
+
+    Image offScreenImage = null;
+
+    @Override
+    public void update(Graphics g) {
+        if(offScreenImage == null){
+            offScreenImage = this.createImage(GAME_WIDTH,GAME_HEIGTH);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0,0,GAME_WIDTH,GAME_HEIGTH);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
+    }
+
 
     @Override
     public void paint(Graphics g) {
